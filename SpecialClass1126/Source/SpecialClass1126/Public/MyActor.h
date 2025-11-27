4fc8e5397f89e6d678a989f3b89b663320b6f532
Yukari <knite7989@gmail.com>
2025-11-27 18:50:16 +0900

    .

--- SpecialClass1126/Source/SpecialClass1126/Public/MyActor.h
+++ SpecialClass1126/Source/SpecialClass1126/Public/MyActor.h
@@ -18,7 +18,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	UPROPERTY(VisibleAnywhere)
+	class USceneComponent* Root;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
